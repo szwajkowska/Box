2db50bbc3bd904081fdbc3b6fa8f1e7b911e01f7
Anna Szwajkowska <szwajkowska.a@gmail.com>
2016-09-29 21:21:28 +0200

    replaceBox

---
+++ src/Box.java
@@ -0,0 +1,84 @@
+public class Box {
+
+    private double boxVolume;
+    private Box centre;
+    private int a = 5;
+
+    public Box() {
+        this.boxVolume = a * a * a;
+
+    }
+
+    public Box(int side) {
+        this.boxVolume = side * side * side;
+
+    }
+
+    public double getBOxVolume() {
+        return boxVolume;
+    }
+
+    public Box getCentre() {
+        return centre;
+    }
+
+    public void putBox(Box innerBox) {
+        if (innerBox.boxVolume < boxVolume && isEmpty()) {
+            centre = innerBox;
+            System.out.println("Umieszczono pudełkow  w pudełku");
+        } else {
+            System.out.println("Nie umieszczono pudełka w pudełku");
+        }
+
+    }
+
+    public Box getBox() {
+        return centre;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) {
+            return "Pudełko o objętości: " + boxVolume;
+        }
+        return "Pudełko o objętośći " + boxVolume + " ma w sobie pudełko o objętośći " + centre.boxVolume;
+    }
+
+    private boolean isEmpty() {
+        if (centre == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public void putBox(int side) {
+        Box box = new Box(side);
+        this.putBox(box);
+    }
+
+    public Box removeBox() {
+        Box innerBox = centre;
+        if (centre != null) {
+            centre = null;
+        }
+        return innerBox;
+    }
+
+    public Box replaceBox(Box newBox) {
+        Box oldCentre = centre;
+        if (isEmpty()) {
+            putBox(newBox);
+            return oldCentre;
+        }
+        else if (newBox !=null && !isEmpty()){
+            if (newBox.boxVolume < boxVolume && newBox.boxVolume > centre.centre.boxVolume && !centre.isEmpty()){
+                putBox(newBox);
+                newBox.putBox(centre.centre);
+            }
+        }
+        return oldCentre;
+    }
+}
+
+
+
